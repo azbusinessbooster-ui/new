3300aa9b36c81d105127b92827b734dbd8df75e5
azbusinessbooster-ui <azbusinessbooster@gmail.com>
2025-12-15 15:07:17 -0700

    Fix body margin style in RootLayout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,4 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margi
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
